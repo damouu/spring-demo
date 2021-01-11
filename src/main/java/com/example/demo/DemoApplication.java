0f3abc78fef8db9d11fe9f35d55e995cf6959bc9
damou_ <mouadsehbaoui@gmail.com>
2021-01-11 20:42:19 +0100

    chore: Student class create a new Student class and add a new route that returns a list of instantiated students #SD-6 Done Unassigned Fixed Done

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,10 +1,15 @@
 package com.example.demo;
 
+import com.example.demo.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
 
 
 @SpringBootApplication
@@ -18,6 +23,18 @@ public class DemoApplication {
     @GetMapping("/hello")
     public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) {
         return String.format("Hello %s!", name);
+    }
+
+    @GetMapping("/students")
+    public List<Student> allStudents() {
+        return List.of(
+                new Student(
+                        2L,
+                        "dede",
+                        LocalDate.of(2000, Month.NOVEMBER, 5),
+                        12,
+                        "petiteTepueDu57@hotmail.fr")
+        );
     }
 
 }
