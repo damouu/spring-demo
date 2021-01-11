a6beb3236b02cc0dde2c2b85abb92ec43e172d78
damou_ <mouadsehbaoui@gmail.com>
2021-01-11 20:42:31 +0100

    chore: Student class create a new Student class and add a new route that returns a list of instantiated students #SD-6 Done Unassigned Fixed Done

---
+++ src/main/java/com/example/demo/student/Student.java
@@ -0,0 +1,59 @@
+package com.example.demo.student;
+
+import java.time.LocalDate;
+
+public class Student {
+    private Long id;
+    private String name;
+    private LocalDate dob;
+    private Integer age;
+    private String email;
+
+    public Student(Long id, String name, LocalDate dob, Integer age, String email) {
+        this.id = id;
+        this.name = name;
+        this.dob = dob;
+        this.age = age;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getDob() {
+        return dob;
+    }
+
+    public void setDob(LocalDate dob) {
+        this.dob = dob;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
