23c5c4c88fc5dd2532e69f23d1a18c0f136c2645
damou_ <mouadsehbaoui@gmail.com>
2021-01-12 15:19:24 +0100

    feature: Post Student create route and feature to post new students into the database #SD-13 Done Unassigned Fixed Done

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class StudentService {
@@ -17,5 +18,13 @@ public class StudentService {
 
     public List<Student> allStudents() {
         return studentRepository.findAll();
+    }
+
+    public void addNewStudent(Student student) {
+        Optional<Student> studentsByEmail = studentRepository.findStudentsByEmail(student.getEmail());
+        if (studentsByEmail.isPresent()) {
+            throw new IllegalStateException("email already taken");
+        }
+        studentRepository.save(student);
     }
 }
