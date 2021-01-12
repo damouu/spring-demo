ff0c8eddfd866d5fa20707c3c485be5f0111d954
damou_ <mouadsehbaoui@gmail.com>
2021-01-12 13:58:29 +0100

    chore:StudentConfig creates StudentConfig to insert data into the database Student #SD-11 Done Unassigned Fixed Done

---
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -0,0 +1,26 @@
+package com.example.demo.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository studentRepository) {
+        return args -> {
+            Student dede = new Student(
+                    1L,
+                    "dede",
+                    LocalDate.of(2000, Month.JULY, 5),
+                    23,
+                    "saladeTomateOgnionsPiments"
+            );
+            studentRepository.saveAndFlush(dede);
+        };
+    }
+}
