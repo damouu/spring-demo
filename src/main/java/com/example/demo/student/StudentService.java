8b17b5ee7ee096f4746a08fa03402274a6c8aa45
damou_ <mouadsehbaoui@gmail.com>
2021-01-12 13:23:04 +0100

    chore:StudentRepository add StudentRepository and extends JpaRepository to Student class #SD-10 Done Unassigned Fixed Done

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,21 +1,21 @@
 package com.example.demo.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
 
 @Service
 public class StudentService {
+
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
     public List<Student> allStudents() {
-        return List.of(
-                new Student(
-                        2L,
-                        "dede",
-                        LocalDate.of(2000, Month.NOVEMBER, 5),
-                        12,
-                        "petiteTepueDu57@hotmail.fr")
-        );
+        return studentRepository.findAll();
     }
 }
