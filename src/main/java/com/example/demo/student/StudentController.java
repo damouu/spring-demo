7e132add9bb644430b9bd8a1b379073008845ae1
damou_ <mouadsehbaoui@gmail.com>
2021-01-12 16:40:04 +0100

    feature: put Student creates a new feature to update an existing student in the database. #SD-15 Done Unassigned Fixed Done

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -31,4 +31,11 @@ public class StudentController {
         studentService.deleteStudent(studentId);
     }
 
+    @PutMapping(path = "{studentId}")
+    public void updateStudent(
+            @PathVariable("studentId") Long studentId,
+            @RequestParam(required = false) String name,
+            @RequestParam(required = false) String email) {
+        studentService.updateStudent(studentId, name, email);
+    }
 }
