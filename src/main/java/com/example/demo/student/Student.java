8c08eb9ebbfbc9a503fde3b95731942d50aad7d8
damou_ <mouadsehbaoui@gmail.com>
2021-01-13 18:45:59 +0100

    chore:Student class update differe student class's constraints with annotations #SD-17 Done Unassigned Fixed Done

--- src/main/java/com/example/demo/student/Student.java
+++ src/main/java/com/example/demo/student/Student.java
@@ -4,17 +4,21 @@ import javax.persistence.*;
 import java.time.LocalDate;
 import java.time.Period;
 
-@Entity
-@Table
+@Entity(name = "student")
+@Table(name = "student", uniqueConstraints = {@UniqueConstraint(name = "student_email_unique", columnNames = "email")})
 public class Student {
     @Id
     @SequenceGenerator(name = "student_sequence", allocationSize = 1, sequenceName = "student_sequence")
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "student_sequence")
+    @Column(updatable = false)
     private Long id;
+    @Column(nullable = false, name = "name")
     private String name;
+    @Column(nullable = false)
     private LocalDate dob;
     @Transient
     private Integer age;
+    @Column(nullable = false, columnDefinition = "TEXT")
     private String email;
 
     public Student(Long id, String name, LocalDate dob, String email) {
