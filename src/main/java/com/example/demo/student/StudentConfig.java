7c39e1d3bb8aed9daddac9f7440a6c8118901d25
damou_ <mouadsehbaoui@gmail.com>
2021-01-13 20:19:21 +0100

    feature:Java Faker create 30 entries into the database at the start of the application #SD-19 Done Unassigned Fixed Done

--- src/main/java/com/example/demo/student/StudentConfig.java
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -1,5 +1,6 @@
 package com.example.demo.student;
 
+import com.github.javafaker.Faker;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,13 +14,16 @@ public class StudentConfig {
     @Bean
     CommandLineRunner commandLineRunner(StudentRepository studentRepository) {
         return args -> {
-            Student dede = new Student(
-                    1L,
-                    "dede",
-                    LocalDate.of(2000, Month.JULY, 5),
-                    "saladeTomateOgnionsPiments"
-            );
-            studentRepository.saveAndFlush(dede);
+            Faker faker = new Faker();
+            for (int i = 1; i < 30; i++) {
+                Student student = new Student(
+                        i,
+                        faker.name().name(),
+                        LocalDate.of(faker.number().numberBetween(1900, 2021), Month.NOVEMBER, faker.number().numberBetween(1, 31)),
+                        faker.internet().emailAddress()
+                );
+                studentRepository.save(student);
+            }
         };
     }
 }
