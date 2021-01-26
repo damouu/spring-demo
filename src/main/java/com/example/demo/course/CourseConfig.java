da74893e48a919733863e8b52a1762ef50383223
damou_ <mouadsehbaoui@gmail.com>
2021-01-26 20:00:42 +0100

    chore: CourseConfig
    Create the CourseConfig.java class that
    creates 7 new courses entries at the start of the application.
    #SD-33 Done Unassigned Fixed Done

---
+++ src/main/java/com/example/demo/course/CourseConfig.java
@@ -0,0 +1,25 @@
+package com.example.demo.course;
+
+import com.github.javafaker.Faker;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CourseConfig {
+
+    @Bean
+    CommandLineRunner lineRunner(CourseRepository courseRepository) {
+        return args -> {
+            Faker faker = new Faker();
+            for (int i = 1; i < 7; i++) {
+                Course course = new Course(
+                        i,
+                        faker.educator().course(),
+                        faker.educator().campus()
+                );
+                courseRepository.save(course);
+            }
+        };
+    }
+}
