6ebc88be09bf2cf4d16673db54f4434407503975
damou_ <mouadsehbaoui@gmail.com>
2021-01-27 19:04:06 +0100

    chore: StudentRepository findStudentById
    Add new findStudentById function to retrieve one student by the given Id.
    #SD-34 Done Unassigned Fixed Done

--- src/main/java/com/example/demo/student/StudentRepository.java
+++ src/main/java/com/example/demo/student/StudentRepository.java
@@ -10,5 +10,6 @@ public interface StudentRepository extends JpaRepository<Student, Integer> {
     Optional<Student> findStudentsByEmail(String email);
 
     Optional<Student> findStudentsById(Integer id);
-    
+
+    Student findStudentById(Integer id);
 }
