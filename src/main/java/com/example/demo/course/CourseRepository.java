d2c3838bdcc22dad3f53305fa9ae0bb13f59d362
damou_ <mouadsehbaoui@gmail.com>
2021-01-28 20:28:18 +0100

    refactor: CourseRepository Class
    Removes unused functions in the CourseRepository interface.
    #SD-37 Done Unassigned Fixed Done

--- src/main/java/com/example/demo/course/CourseRepository.java
+++ src/main/java/com/example/demo/course/CourseRepository.java
@@ -3,11 +3,7 @@ package com.example.demo.course;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface CourseRepository extends JpaRepository<Course, Integer> {
-    Optional<Course> findCourseByDepartment(String s);
-
-    Course findCourseById(Integer id);
+    
 }
