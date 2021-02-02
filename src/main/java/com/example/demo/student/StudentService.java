dec076269b96a804de4fcbb1e7276b181dd1a13b
damou_ <mouadsehbaoui@gmail.com>
2021-02-02 23:27:11 +0100

    Refactor: StudentService
    Refactor the Type and the name of the parameter passed to be an UUID named studentUiid.
    #SD-39 Done Unassigned Fixed Done

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,18 +1,14 @@
 package com.example.demo.student;
 
-import com.example.demo.course.Course;
 import com.example.demo.course.CourseRepository;
-import com.github.javafaker.Faker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.ws.rs.core.Response;
 import java.net.URI;
-import java.time.LocalDate;
-import java.time.Month;
-import java.time.Period;
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class StudentService {
@@ -27,14 +23,13 @@ public class StudentService {
         this.courseRepository = courseRepository;
     }
 
-    public List<Student> allStudents(Optional<Integer> queryParam) {
-        List<Student> students = studentRepository.findAll();
-        if (queryParam.isEmpty()) {
-            return students;
-        }
-        students.stream().iterator().forEachRemaining(student -> student.setAge(Period.between(student.getDob(), LocalDate.now()).getYears()));
-        students.removeIf(student -> !queryParam.get().equals(student.getAge()));
-        return students;
+    public List<Student> allStudents() {
+        return studentRepository.findAll();
+    }
+
+    public Student getStudent(UUID studentUuid) {
+        Optional<Student> optionalStudent = studentRepository.findStudentByUuid(studentUuid);
+        return optionalStudent.orElse(null);
     }
 
     public Response addNewStudent(Student student) {
@@ -46,19 +41,19 @@ public class StudentService {
         return Response.ok(student).status(201).contentLocation(URI.create("http://localhost:8080/api/student/" + student.getId())).build();
     }
 
-    public Response deleteStudent(Integer studentId) {
+    public Response deleteStudent(UUID studentUuid) {
         try {
-            Optional<Student> student = studentRepository.findStudentsById(studentId);
-            studentRepository.deleteById(studentId);
+            Optional<Student> student = studentRepository.findStudentByUuid(studentUuid);
+            studentRepository.delete(student.get());
             return Response.noContent().status(204).build();
         } catch (Exception exception) {
             return Response.noContent().status(404).language(String.valueOf(exception)).build();
         }
     }
 
-    public Response updateStudent(Integer studentId, Student student) {
-        if (studentRepository.findStudentsById(studentId).isPresent()) {
-            Student student1 = studentRepository.findStudentsById(studentId).get();
+    public Response updateStudent(UUID studentUuid, Student student) {
+        if (studentRepository.findStudentByUuid(studentUuid).isPresent()) {
+            Student student1 = studentRepository.findStudentByUuid(studentUuid).get();
             student1.setName(student.getName());
             student1.setDob(student.getDob());
             student1.setEmail(student.getEmail());
@@ -66,19 +61,5 @@ public class StudentService {
             return Response.ok(student1).status(204).build();
         }
         return Response.notModified().status(404).build();
-    }
-
-    public Response findById(Integer studentId) {
-        Course course = new Course(50, "japanese_kanji", "language_japanese");
-        Faker faker = new Faker();
-        Student student = new Student(1000, "dede", LocalDate.of(faker.number().numberBetween(1900, 2021), Month.NOVEMBER, faker.number().numberBetween(1, 31)), "deed@gmail.com");
-        course.getStudents().add(student);
-        student.getCourses().add(course);
-        courseRepository.save(course);
-        Optional<Student> optionalStudent = studentRepository.findStudentsById(studentId);
-        if (optionalStudent.isPresent()) {
-            return Response.ok().entity(optionalStudent.get()).build();
-        }
-        return Response.status(404).build();
     }
 }
