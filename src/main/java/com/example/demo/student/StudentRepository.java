137059257cc67672722efacc255929f0d4fcc609
damou_ <mouadsehbaoui@gmail.com>
2021-02-02 23:24:15 +0100

    feature: StudentRepository
    Removes an unused methods and add a new method to search a student by the provided UUID.
    #SD-39 Done Unassigned Fixed Done

--- src/main/java/com/example/demo/student/StudentRepository.java
+++ src/main/java/com/example/demo/student/StudentRepository.java
@@ -4,12 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
+import java.util.UUID;
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Integer> {
     Optional<Student> findStudentsByEmail(String email);
 
-    Optional<Student> findStudentsById(Integer id);
-
-    Student findStudentById(Integer id);
+    Optional<Student> findStudentByUuid(UUID uuid);
 }
