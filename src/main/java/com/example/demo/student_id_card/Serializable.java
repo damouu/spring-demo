53332c34225328bb6a4e998990dd84d2b9ccf88f
damou_ <mouadsehbaoui@gmail.com>
2021-02-02 22:05:10 +0100

    New chore Serializable.java #SD-38 Done Unassigned Fixed Done

--- src/main/java/com/example/demo/student_id_card/Serializable.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.student_id_card;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface Serializable extends JpaRepository<StudentIdCard, Integer> {
-}
