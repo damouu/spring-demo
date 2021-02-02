d26f0fd213662668f34da876c9886d71db480ae8
damou_ <mouadsehbaoui@gmail.com>
2021-02-02 22:09:31 +0100

    chore: StudentIdCardController
    creates the StudentIdCardController class.
    
    #SD-38 Done Unassigned Fixed Done

---
+++ src/main/java/com/example/demo/student_id_card/StudentIdCardController.java
@@ -0,0 +1,52 @@
+package com.example.demo.student_id_card;
+
+import org.jboss.resteasy.annotations.jaxrs.PathParam;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.Response;
+import java.util.List;
+import java.util.UUID;
+
+@Validated
+@Controller
+@Path("api/studentCard")
+public class StudentIdCardController {
+
+    private StudentIdCardService studentIdCardService;
+
+    @Autowired
+    public StudentIdCardController(StudentIdCardService studentIdCardService) {
+        this.studentIdCardService = studentIdCardService;
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON_VALUE)
+    public List<StudentIdCard> getStudentIdCards() {
+        return studentIdCardService.getStudentIdCards();
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON_VALUE)
+    @Path("/{studentCardNumber}")
+    public StudentIdCard getStudentIdCard(@PathParam("studentCardNumber") UUID studentCardNumber) {
+        return studentIdCardService.getStudentIdCard(studentCardNumber);
+    }
+
+    @DELETE
+    @Produces(MediaType.APPLICATION_JSON_VALUE)
+    @Path("/{studentCardNumber}")
+    public Response deleteStudentIdCard(@PathParam("studentCardNumber") UUID studentCardNumber) {
+        return studentIdCardService.deleteStudentIdCard(studentCardNumber);
+    }
+
+    @POST
+    @Produces(MediaType.APPLICATION_JSON_VALUE)
+    public Response createStudentIdCard(@QueryParam("studentId") Integer studentId) {
+        return studentIdCardService.createStudentIdCard(studentId);
+    }
+
+}
