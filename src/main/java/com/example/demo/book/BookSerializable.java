d858096e64126e51fdddc2759454de1033f89c25
damou_ <mouadsehbaoui@gmail.com>
2021-02-05 10:38:37 +0100

    refactor: BookSerializable findByUuid
    Add an findByUuid function int the BookSerializable interface.
    #SD-40 Done Unassigned Fixed Done

--- src/main/java/com/example/demo/book/BookSerializable.java
+++ src/main/java/com/example/demo/book/BookSerializable.java
@@ -3,6 +3,10 @@ package com.example.demo.book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+import java.util.UUID;
+
 @Repository
 public interface BookSerializable extends JpaRepository<Book, Integer> {
+    Optional<Book> findByUuid(UUID uuid);
 }
