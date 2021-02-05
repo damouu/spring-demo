537d663b14ccb4717e0bdc5a2004cdff7b95157e
damou_ <mouadsehbaoui@gmail.com>
2021-02-05 11:46:09 +0100

    refactor: CourseRepository findByUuid
    Add an findByUuid function int the CourseRepository interface.
    
    #SD-41 Done Unassigned Fixed Done

--- src/main/java/com/example/demo/course/CourseRepository.java
+++ src/main/java/com/example/demo/course/CourseRepository.java
@@ -3,7 +3,12 @@ package com.example.demo.course;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.validation.constraints.NotNull;
+import java.util.Optional;
+import java.util.UUID;
+
 @Repository
 public interface CourseRepository extends JpaRepository<Course, Integer> {
-    
+
+    Optional<Course> findByUuid(@NotNull UUID uuid);
 }
