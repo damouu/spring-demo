820185f9bd32aab113f8aa19971a4865fe05bd5d
damou_ <mouadsehbaoui@gmail.com>
2021-02-08 16:57:39 +0100

    refactor: BookSerializable findAllByTotalPages
    Creates a new function in the BookSerializable class for the sake of the query params option "total Pages".
    
    #SD-45 Done Unassigned Fixed Done

--- src/main/java/com/example/demo/book/BookSerializable.java
+++ src/main/java/com/example/demo/book/BookSerializable.java
@@ -3,10 +3,13 @@ package com.example.demo.book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface BookSerializable extends JpaRepository<Book, Integer> {
     Optional<Book> findByUuid(UUID uuid);
+
+    Optional<List<Book>> findAllByTotalPages(Optional<Integer> integer);
 }
