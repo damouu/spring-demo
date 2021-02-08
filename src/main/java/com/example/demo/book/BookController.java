5de28fe5eaf0d11f2c43f61276c2afe0570f6994
damou_ <mouadsehbaoui@gmail.com>
2021-02-08 18:53:54 +0100

    feature: BookController findAllByGenreAndTotalPages
    Add the function findAllByGenreAndTotalPages to retrieve books by the given genre and the total pages by passing both queryParams totalPages and Genre.
    
    #SD-46 Done Unassigned Fixed Done

--- src/main/java/com/example/demo/book/BookController.java
+++ src/main/java/com/example/demo/book/BookController.java
@@ -9,7 +9,7 @@ import org.springframework.validation.annotation.Validated;
 import javax.validation.Valid;
 import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
-import java.util.List;
+import java.util.Collection;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -27,9 +27,9 @@ public class BookController {
 
     @GET
     @Produces(MediaType.APPLICATION_JSON_VALUE)
-    public List<Book> getBooks(@DefaultValue("0") @QueryParam("page") int page, @DefaultValue("5") @QueryParam("size") int size,
-                               @QueryParam("totalPages") Optional<Integer> totalPages) {
-        return bookService.getBooks(page, size, totalPages);
+    public Collection<Book> getBooks(@DefaultValue("0") @QueryParam("page") int page, @DefaultValue("5") @QueryParam("size") int size,
+                                     @QueryParam("totalPages") Optional<Integer> totalPages, @QueryParam("genre") Optional<String> genre) {
+        return bookService.getBooks(page, size, totalPages, genre);
     }
 
     @GET
