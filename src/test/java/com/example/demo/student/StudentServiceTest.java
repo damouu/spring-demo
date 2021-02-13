b1d447acc573d6005fd2ea6e3b7ad15471a34640
damou_ <mouadsehbaoui@gmail.com>
2021-02-13 17:15:24 +0100

    understanding the Unit test using mockito and the argument captor.

--- src/test/java/com/example/demo/student/StudentServiceTest.java
+++ src/test/java/com/example/demo/student/StudentServiceTest.java
@@ -4,10 +4,13 @@ package com.example.demo.student;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
 import java.time.LocalDate;
+import java.util.Optional;
 import java.util.UUID;
 
 class StudentServiceTest {
@@ -25,15 +28,19 @@ class StudentServiceTest {
     }
 
     @Test
-    void addNewStudent() {
+    void getStudent() {
         Student student = new Student(1, UUID.randomUUID(), "dede", LocalDate.of(2020, 12, 12),
                 "dede@fdp.com");
-        Assertions.assertEquals(student, student);
-        Assertions.assertNotNull(student);
+        Mockito.when(studentRepository.findStudentByUuid(student.getUuid())).thenReturn(Optional.of(student));
+        Mockito.when(studentRepository.findById(student.getId())).thenReturn(Optional.of(student));
+        Assertions.assertEquals(student, studentService.getStudent(student.getUuid()));
     }
 
     @Test
     void deleteStudent() {
+        Student student = new Student(1, UUID.randomUUID(), "dede", LocalDate.of(2020, 12, 12),
+                "dede@fdp.com");
+        ArgumentCaptor<Student> captor = ArgumentCaptor.forClass(Student.class);
     }
 
 }
