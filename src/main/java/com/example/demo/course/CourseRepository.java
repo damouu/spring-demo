d4fc9a00a4cb7489e2321db4cdd6f3620f9a1e1e
damou_ <mouadsehbaoui@gmail.com>
2021-02-15 15:41:21 +0100

    CourseRepository findByCampusContaining
    Adds a new function to retrieve all the courses containing the same department name as the passed query param value.
    
    #5SD-59 Done Unassigned Fixed Done

--- src/main/java/com/example/demo/course/CourseRepository.java
+++ src/main/java/com/example/demo/course/CourseRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import javax.validation.constraints.NotNull;
+import java.util.Collection;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -11,4 +12,8 @@ import java.util.UUID;
 public interface CourseRepository extends JpaRepository<Course, Integer> {
 
     Optional<Course> findByUuid(@NotNull UUID uuid);
+
+    // the "containing" keyword is used as the LIKE keyword in SQL syntax.
+    // Containing: select ... like %:place%
+    Optional<Collection<Course>> findByCampusContaining(String department);
 }
