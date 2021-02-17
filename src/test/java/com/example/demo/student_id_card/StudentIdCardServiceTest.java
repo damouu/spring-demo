28de6d09844af251814670954c9fc87bf1555d31
damou_ <mouadsehbaoui@gmail.com>
2021-02-17 23:12:57 +0100

    StudentIdCardServiceTest
    Add the StudentIdCardServiceTest with the
    Unit test for the getStudentIdCards function.
    
    #SD-63 Done Unassigned Fixed Done

---
+++ src/test/java/com/example/demo/student_id_card/StudentIdCardServiceTest.java
@@ -0,0 +1,48 @@
+package com.example.demo.student_id_card;
+
+import com.example.demo.student.StudentRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+class StudentIdCardServiceTest {
+
+    @Mock
+    private StudentIdCardSerializable studentIdCardSerializable;
+
+    private StudentIdCardService studentIdCardService;
+
+    private StudentRepository studentRepository;
+
+    @Captor
+    private ArgumentCaptor<StudentIdCard> argumentCaptor;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+        studentIdCardService = new StudentIdCardService(studentIdCardSerializable, studentRepository);
+    }
+
+    @Test
+    void getStudentIdCards() {
+        List<StudentIdCard> studentIdCards = new ArrayList<>();
+        StudentIdCard studentIdCard = new StudentIdCard(1, UUID.randomUUID());
+        studentIdCards.add(studentIdCard);
+        Mockito.when(studentIdCardService.getStudentIdCards()).thenReturn(studentIdCards);
+        List<StudentIdCard> studentIdCards1 = studentIdCardService.getStudentIdCards();
+        Assertions.assertNotNull(studentIdCards1);
+    }
+
+    @Test
+    void getStudentIdCard() {
+    }
+
+    @Test
+    void createStudentIdCard() {
+    }
+}
