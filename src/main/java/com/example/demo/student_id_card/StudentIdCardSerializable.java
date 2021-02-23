8b01b4d71744562031273b00ab8e38fd2581eddf
damou_ <mouadsehbaoui@gmail.com>
2021-02-23 19:29:15 +0100

    StudentIdCardSerializable Repository annotation
    Add to the StudentIdCardSerializable a @Repository spring framework annotation so this interface can be injected.
    
    #SD-64 Done Unassigned Fixed Done

--- src/main/java/com/example/demo/student_id_card/StudentIdCardSerializable.java
+++ src/main/java/com/example/demo/student_id_card/StudentIdCardSerializable.java
@@ -1,10 +1,12 @@
 package com.example.demo.student_id_card;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface StudentIdCardSerializable extends JpaRepository<StudentIdCard, Integer> {
 
     Optional<StudentIdCard> findStudentIdCardByUuid(UUID studentCardNumber);
