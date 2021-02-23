bc6e1b45a404e8c9ec84e7192deb0179ad034832
damou_ <mouadsehbaoui@gmail.com>
2021-02-23 19:27:12 +0100

    Student Entity add OneToMany Book property.
    Adds a new property OneToMany Books to link with the Book Entity with a foreign key.adds also the getters and setters for this new property.
    
    In order for this class to be used with the Book entity, this class implements Serializable.
    
    #SD-64 Done Unassigned Fixed Done

--- src/main/java/com/example/demo/student_id_card/StudentIdCard.java
+++ src/main/java/com/example/demo/student_id_card/StudentIdCard.java
@@ -1,5 +1,6 @@
 package com.example.demo.student_id_card;
 
+import com.example.demo.book.Book;
 import com.example.demo.student.Student;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnore;
@@ -8,12 +9,14 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+import java.util.Set;
 import java.util.UUID;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @Entity(name = "Student_id_card")
-@Table(name = "Student_id_card", uniqueConstraints = {@UniqueConstraint(name = "uuid", columnNames = "uuid")})
-public class StudentIdCard {
+@Table(name = "Student_id_card", uniqueConstraints = @UniqueConstraint(columnNames = {"uuid", "uuid"}, name = "student_card_uuid_unique"))
+public class StudentIdCard implements Serializable {
     @Id
     @Column(unique = true, updatable = false, nullable = false)
     @SequenceGenerator(name = "student_id_card_sequence", allocationSize = 1, sequenceName = "student_id_card_sequence")
@@ -27,6 +30,9 @@ public class StudentIdCard {
     @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "student_id", referencedColumnName = "id")
     private Student student;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "studentIdCard", fetch = FetchType.EAGER)
+    private Set<Book> books;
 
     @JsonCreator
     public StudentIdCard(@JsonProperty("id") Integer id, @JsonProperty("uuid") UUID uuid) {
@@ -61,5 +67,22 @@ public class StudentIdCard {
     @JsonIgnore
     public void setStudent(Student student) {
         this.student = student;
+    }
+
+    public UUID getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(UUID uuid) {
+        this.uuid = uuid;
+    }
+
+    @JsonIgnore
+    public Set<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(Set<Book> books) {
+        this.books = books;
     }
 }
