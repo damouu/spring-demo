351eaa548b44c5adac596d062d13c7680cf4c8e4
damou_ <mouadsehbaoui@gmail.com>
2021-02-24 16:56:02 +0100

    refactor

--- src/test/java/com/example/demo/student_id_card/StudentIdCardServiceTest.java
+++ src/test/java/com/example/demo/student_id_card/StudentIdCardServiceTest.java
@@ -13,7 +13,7 @@ import java.util.UUID;
 class StudentIdCardServiceTest {
 
     @Mock
-    private StudentIdCardSerializable studentIdCardSerializable;
+    private StudentIdCardRepository studentIdCardRepository;
 
     private StudentIdCardService studentIdCardService;
 
@@ -25,7 +25,7 @@ class StudentIdCardServiceTest {
     @BeforeEach
     void setUp() {
         MockitoAnnotations.initMocks(this);
-        studentIdCardService = new StudentIdCardService(studentIdCardSerializable, studentRepository);
+        studentIdCardService = new StudentIdCardService(studentIdCardRepository, studentRepository);
     }
 
     @Test
