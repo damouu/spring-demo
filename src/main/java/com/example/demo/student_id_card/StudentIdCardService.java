4fcbfbba8c61a6c96821c3979f55b9310014fc32
damou_ <mouadsehbaoui@gmail.com>
2021-02-24 16:55:46 +0100

    refactor StudentIdCardService
    refactor with new class's names
    
    #SD-65 Done Unassigned Fixed Done

--- src/main/java/com/example/demo/student_id_card/StudentIdCardService.java
+++ src/main/java/com/example/demo/student_id_card/StudentIdCardService.java
@@ -14,29 +14,29 @@ import java.util.UUID;
 @Service
 public class StudentIdCardService {
 
-    private StudentIdCardSerializable studentIdCardSerializable;
+    private final StudentIdCardRepository studentIdCardRepository;
 
-    private StudentRepository studentRepository;
+    private final StudentRepository studentRepository;
 
     @Autowired
-    public StudentIdCardService(StudentIdCardSerializable studentIdCardSerializable, StudentRepository studentRepository) {
-        this.studentIdCardSerializable = studentIdCardSerializable;
+    public StudentIdCardService(StudentIdCardRepository studentIdCardRepository, StudentRepository studentRepository) {
+        this.studentIdCardRepository = studentIdCardRepository;
         this.studentRepository = studentRepository;
     }
 
     public List<StudentIdCard> getStudentIdCards() {
-        return studentIdCardSerializable.findAll();
+        return studentIdCardRepository.findAll();
     }
 
     public Student getStudentIdCard(UUID studentCardNumber) {
-        Optional<StudentIdCard> studentIdCardByCardNumber = studentIdCardSerializable.findStudentIdCardByUuid(studentCardNumber);
+        Optional<StudentIdCard> studentIdCardByCardNumber = studentIdCardRepository.findStudentIdCardByUuid(studentCardNumber);
         return studentIdCardByCardNumber.map(StudentIdCard::getStudent).orElse(null);
     }
 
     public Response deleteStudentIdCard(UUID studentCardNumber) {
-        Optional<StudentIdCard> studentIdCard = studentIdCardSerializable.findStudentIdCardByUuid(studentCardNumber);
+        Optional<StudentIdCard> studentIdCard = studentIdCardRepository.findStudentIdCardByUuid(studentCardNumber);
         if (studentIdCard.isPresent()) {
-            studentIdCardSerializable.delete(studentIdCard.get());
+            studentIdCardRepository.delete(studentIdCard.get());
             return Response.ok(studentIdCard.get()).status(204).build();
         }
         return Response.noContent().status(404).build();
@@ -45,10 +45,10 @@ public class StudentIdCardService {
     public Response createStudentIdCard(UUID studentUuid) {
         Optional<Student> optionalStudent = studentRepository.findStudentByUuid(studentUuid);
         if (optionalStudent.isPresent()) {
-            int studentIdCardLast = Math.toIntExact(studentIdCardSerializable.count());
+            int studentIdCardLast = Math.toIntExact(studentIdCardRepository.count());
             StudentIdCard studentIdCard = new StudentIdCard(studentIdCardLast + 1, UUID.randomUUID());
             studentIdCard.setStudent(optionalStudent.get());
-            studentIdCardSerializable.save(studentIdCard);
+            studentIdCardRepository.save(studentIdCard);
             return Response.ok(studentIdCard).status(202).contentLocation(URI.create("http://localhost/api/studentCard/" + studentIdCard.getuuid())).build();
         }
         return Response.notModified().build();
