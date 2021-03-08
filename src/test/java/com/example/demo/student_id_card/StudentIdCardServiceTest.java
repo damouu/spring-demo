37379a711553c90927183850148cd8ccbd700dcb
damou_ <mouadsehbaoui@gmail.com>
2021-03-08 20:21:53 +0100

    commits file changes

--- src/test/java/com/example/demo/student_id_card/StudentIdCardServiceTest.java
+++ src/test/java/com/example/demo/student_id_card/StudentIdCardServiceTest.java
@@ -1,14 +1,12 @@
 package com.example.demo.student_id_card;
 
 import com.example.demo.student.StudentRepository;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.*;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 class StudentIdCardServiceTest {
 
@@ -30,12 +28,7 @@ class StudentIdCardServiceTest {
 
     @Test
     void getStudentIdCards() {
-        List<StudentIdCard> studentIdCards = new ArrayList<>();
-        StudentIdCard studentIdCard = new StudentIdCard(1, UUID.randomUUID());
-        studentIdCards.add(studentIdCard);
-        Mockito.when(studentIdCardService.getStudentIdCards()).thenReturn(studentIdCards);
-        List<StudentIdCard> studentIdCards1 = studentIdCardService.getStudentIdCards();
-        Assertions.assertNotNull(studentIdCards1);
+
     }
 
     @Test
