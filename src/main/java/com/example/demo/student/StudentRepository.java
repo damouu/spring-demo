d14bd3aa45c93898479c94384263cf4169e735ac
damou_ <mouadsehbaoui@gmail.com>
2021-03-09 15:43:58 +0100

    refactor: StudentRepository findStudentsByEmail
    refactor the findStudentsByEmail query , specifies a NotNull constraints in the signature and specifies the data type needed.
    
    #SD-86 Done Unassigned Fixed Done

--- src/main/java/com/example/demo/student/StudentRepository.java
+++ src/main/java/com/example/demo/student/StudentRepository.java
@@ -2,13 +2,17 @@ package com.example.demo.student;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.validation.annotation.Validated;
 
+import javax.validation.constraints.NotNull;
 import java.util.Optional;
 import java.util.UUID;
 
+@Validated
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Integer> {
-    Optional<Student> findStudentsByEmail(String email);
+    
+    Optional<Student> findStudentsByEmail(@NotNull String email);
 
-    Optional<Student> findStudentByUuid(UUID uuid);
+    Optional<Student> findStudentByUuid(@NotNull UUID uuid);
 }
