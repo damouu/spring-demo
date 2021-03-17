03440683d4c10cc761c1c286e553ad7a6a4c1bb4
damou_ <mouadsehbaoui@gmail.com>
2021-03-17 13:51:31 +0100

    Adds the lombok annotations getters / setters for the Student Class's properties.
    #SD-92 Done Unassigned Fixed Done

--- src/main/java/com/example/demo/student/Student.java
+++ src/main/java/com/example/demo/student/Student.java
@@ -10,12 +10,13 @@ import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
-import java.time.Period;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.UUID;
@@ -26,36 +27,52 @@ import java.util.UUID;
         @UniqueConstraint(columnNames = {"email", "email"}, name = "student_email_unique"),
         @UniqueConstraint(columnNames = {"uuid", "uuid"}, name = "student_uuid_unique")
 })
+
 public class Student {
     @Id
     @SequenceGenerator(name = "student_sequence", allocationSize = 1, sequenceName = "student_sequence")
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "student_sequence")
     @Column(updatable = false)
+    @Getter(onMethod = @__(@JsonIgnore)) // generate the getter with the specific annotation.
+    @Setter
     private Integer id;
 
     @Column(nullable = false, columnDefinition = "UUID", name = "uuid")
+    @Getter
+    @Setter
     private UUID uuid;
 
     @Column(nullable = false, name = "name")
     @NotNull
+    @Getter
+    @Setter
     private String name;
 
     @Column(nullable = false)
     @JsonDeserialize(using = LocalDateDeserializer.class)
     @JsonSerialize(using = LocalDateSerializer.class)
     @NotNull
+    @Getter
+    @Setter
     private LocalDate dob;
 
     @Transient
+    @Getter
+    @Setter
     private Integer age;
 
     @Column(nullable = false, columnDefinition = "TEXT")
     @NotNull
     @Email
+    @Getter
+    @Setter
     private String email;
 
     @OneToOne(mappedBy = "student")
+    @Getter(onMethod = @__(@JsonIgnore))
+    @Setter
     private StudentIdCard studentIdCard;
+
 
     @ManyToMany(fetch = FetchType.EAGER,
             cascade = {
@@ -65,7 +82,10 @@ public class Student {
     @JoinTable(name = "course_student",
             joinColumns = {@JoinColumn(name = "student_id")},
             inverseJoinColumns = {@JoinColumn(name = "course_id")})
+    @Getter(onMethod = @__(@JsonIgnore)) // generate the getter with the specific annotation.
+    @Setter
     protected Set<Course> courses = new HashSet<>();
+
 
     @JsonCreator
     public Student(@JsonProperty("id") Integer id, @JsonProperty("uuid") UUID uuid, @JsonProperty("name") String name, @JsonProperty("dob") LocalDate dob, @JsonProperty("email") String email) {
@@ -78,72 +98,4 @@ public class Student {
 
     public Student() {
     }
-
-    @JsonIgnore
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = Math.toIntExact(id);
-    }
-
-    public UUID getUuid() {
-        return uuid;
-    }
-
-    public void setUuid(UUID uuid) {
-        this.uuid = uuid;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public LocalDate getDob() {
-        return dob;
-    }
-
-    public void setDob(LocalDate dob) {
-        this.dob = dob;
-    }
-
-    public Integer getAge() {
-        return Period.between(this.dob, LocalDate.now()).getYears();
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    @JsonIgnore
-    public StudentIdCard getStudentIdCard() {
-        return studentIdCard;
-    }
-
-    public void setStudentIdCard(StudentIdCard studentIdCard) {
-        this.studentIdCard = studentIdCard;
-    }
-
-    @JsonIgnore
-    public Set<Course> getCourses() {
-        return courses;
-    }
-
-    public void setCourses(Set<Course> courses) {
-        this.courses = courses;
-    }
-
 }
