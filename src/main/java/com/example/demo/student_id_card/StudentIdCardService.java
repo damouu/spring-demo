40a768933d624e4660fcae09015a000363a497cc
damou_ <mouadsehbaoui@gmail.com>
2021-03-18 16:39:16 +0100

    refactor: rename function StudentServiceTest
    rename the function createStudentIdCard to postStudentIdCard.
    
    #SD-94 Done Unassigned Fixed Done

--- src/main/java/com/example/demo/student_id_card/StudentIdCardService.java
+++ src/main/java/com/example/demo/student_id_card/StudentIdCardService.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityNotFoundException;
 import javax.ws.rs.core.Response;
 import java.net.URI;
 import java.util.UUID;
@@ -42,9 +43,9 @@ public class StudentIdCardService {
         return Response.ok().status(204).build();
     }
 
-    public Response createStudentIdCard(UUID studentUuid) {
-        Student student = this.studentRepository.findStudentByUuid(studentUuid).orElseThrow(() -> new IllegalStateException("student does not exist"));
-        StudentIdCard studentIdCard = new StudentIdCard(Math.toIntExact(studentIdCardRepository.count()) + 1, UUID.randomUUID());
+    public Response postStudentIdCard(UUID studentUuid) {
+        Student student = this.studentRepository.findStudentByUuid(studentUuid).orElseThrow(() -> new EntityNotFoundException("student does not exist"));
+        StudentIdCard studentIdCard = new StudentIdCard(UUID.randomUUID());
         studentIdCard.setStudent(student);
         studentIdCardRepository.save(studentIdCard);
         return Response.ok().status(202).contentLocation(URI.create("http://localhost:8083/api/studentCard/" + studentIdCard.getUuid())).build();
