f8c7e54f30387ae3e10cedf1c5d9e3502f98efbe
damou_ <mouadsehbaoui@gmail.com>
2021-03-22 17:55:19 +0100

    refactor: StudentController NO RESTEASY
    refactor every route to re implement the SpringBoot implementation.
    
    #SD-98 Done Unassigned Fixed Done

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -2,61 +2,49 @@ package com.example.demo.student;
 
 import lombok.Data;
 import org.springframework.http.MediaType;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import javax.ws.rs.*;
-import javax.ws.rs.core.Response;
 import java.util.Collection;
 import java.util.UUID;
 
-@Validated
-@Controller
 @Data
-@Path("api/student")
+@Validated
+@RestController
+@RequestMapping("api/student")
 public class StudentController {
 
     private final StudentService studentService;
 
-    @GET
-    @Produces(MediaType.APPLICATION_JSON_VALUE)
-    public Collection<Student> allStudents(@DefaultValue("0") @QueryParam("page") int page, @DefaultValue("5") @QueryParam("size") int sort) {
-        return studentService.allStudents(page, sort);
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public Collection<Student> allStudents(@RequestParam(name = "page", required = false, defaultValue = "0") Integer page, @RequestParam(name = "size", required = false, defaultValue = "5") Integer size) {
+        return studentService.allStudents(page, size);
     }
 
-    @GET
-    @Path("/{studentUuid}")
-    @Produces(MediaType.APPLICATION_JSON_VALUE)
-    public Student getStudent(@PathParam("studentUuid") UUID studentUuid) {
+    @GetMapping(path = "/{studentUuid}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Student getStudent(@PathVariable("studentUuid") UUID studentUuid) {
         return studentService.getStudent(studentUuid);
     }
 
-    @POST
-    @Consumes(MediaType.APPLICATION_JSON_VALUE)
-    @Produces(MediaType.APPLICATION_JSON_VALUE)
-    public Student postStudent(@Valid Student student) {
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> postStudent(@Valid @RequestBody Student student) {
         return studentService.postStudent(student);
     }
 
-    @DELETE
-    @Path("/{studentUuid}")
-    public Response deleteStudent(@PathParam("studentUuid") UUID studentUuid) {
+    @DeleteMapping(path = "/{studentUuid}")
+    public ResponseEntity<?> deleteStudent(@PathVariable("studentUuid") UUID studentUuid) {
         return studentService.deleteStudent(studentUuid);
     }
 
-    @PUT
-    @Path("/{studentUuid}")
-    @Consumes(MediaType.APPLICATION_JSON_VALUE)
-    @Produces(MediaType.APPLICATION_JSON_VALUE)
-    public Student updateStudent(@PathParam("studentUuid") UUID studentUuid, @Valid Student student) {
+    @PutMapping(path = "/{studentUuid}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Student updateStudent(@PathVariable("studentUuid") UUID studentUuid, @RequestBody Student student) {
         return studentService.updateStudent(studentUuid, student);
     }
 
-    @GET
-    @Produces(MediaType.APPLICATION_JSON_VALUE)
-    @Path("/{studentUuid}/course")
-    public Response getCourseStudent(@PathParam("studentUuid") UUID studentUuid) {
+    @GetMapping(path = "/{studentUuid}/course", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> getCourseStudent(@PathVariable("studentUuid") UUID studentUuid) {
         return studentService.getCourseStudent(studentUuid);
     }
 }
