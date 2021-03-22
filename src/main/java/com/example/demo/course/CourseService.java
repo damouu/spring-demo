6165e161d9c0816e4d45315ab14e0a5efb465e2d
damou_ <mouadsehbaoui@gmail.com>
2021-03-22 19:02:03 +0100

    refactor: CourseService NO RESTEASY
    remove the usage of the RESTEASY implementation to return to the SpringBoot implementation.
    
    #SD-99 Done Unassigned Fixed Done

--- src/main/java/com/example/demo/course/CourseService.java
+++ src/main/java/com/example/demo/course/CourseService.java
@@ -6,17 +6,18 @@ import lombok.Data;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import javax.persistence.EntityNotFoundException;
-import javax.ws.rs.core.Response;
 import java.net.URI;
 import java.util.Collection;
-import java.util.Optional;
 import java.util.UUID;
 
-@Service
 @Data
+@Service
 public class CourseService {
 
     private final CourseRepository courseRepository;
@@ -33,77 +34,78 @@ public class CourseService {
         return courses.toList();
     }
 
-    public Response postCourse(Course course) {
+    public ResponseEntity<?> postCourse(Course course) {
         course.setUuid(UUID.randomUUID());
         courseRepository.save(course);
-        return Response.ok(course).status(201).contentLocation(URI.create("http://localhost:8083/api/course/" + course.getUuid())).build();
+        return ResponseEntity.status(201).location(URI.create("http://localhost:8083/api/course/" + course.getUuid())).build();
     }
 
-    public Response deleteCourse(UUID courseUuid) {
-        Course course = courseRepository.findByUuid(courseUuid).orElseThrow(() -> new IllegalStateException("course does not exist"));
+    public ResponseEntity<?> deleteCourse(UUID courseUuid) {
+        Course course = courseRepository.findByUuid(courseUuid).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "course does not exist"));
         courseRepository.delete(course);
-        return Response.ok(course).status(204).build();
+        return ResponseEntity.status(204).build();
     }
 
-    public Response updateCourse(UUID courseUuid, Course course) {
-        Course optionalCourse = courseRepository.findByUuid(courseUuid).orElseThrow(() -> new IllegalStateException("course does not exist"));
+    public ResponseEntity<?> updateCourse(UUID courseUuid, Course course) {
+        Course optionalCourse = courseRepository.findByUuid(courseUuid).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "course does not exist"));
         optionalCourse.setName(course.getName());
         optionalCourse.setCampus(course.getCampus());
+        optionalCourse.setUniversity(course.getUniversity());
         courseRepository.save(optionalCourse);
-        return Response.ok(course).status(204).location(URI.create("http://localhost:8083/api/course/" + course.getUuid())).build();
+        return ResponseEntity.status(204).location(URI.create("http://localhost:8083/api/course/" + course.getUuid())).build();
     }
 
-    public Response postStudentCourse(UUID courseUuid, UUID studentUuid) {
-        Course course = courseRepository.findByUuid(courseUuid).orElseThrow(() -> new IllegalStateException("course does not exist"));
-        Student student = studentRepository.findStudentByUuid(studentUuid).orElseThrow(() -> new IllegalStateException("student does not exist"));
+    public ResponseEntity<?> postStudentCourse(UUID courseUuid, UUID studentUuid) {
+        Course course = courseRepository.findByUuid(courseUuid).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "course does not exist"));
+        Student student = studentRepository.findStudentByUuid(studentUuid).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "student does not exist"));
         if (course.getStudents().stream().anyMatch(student1 -> student1.getUuid().equals(student.getUuid()))) {
-            return Response.noContent().status(406).build();
+            return ResponseEntity.noContent().build();
         } else {
             student.getCourses().add(course);
             this.studentRepository.save(student);
-            return Response.ok(course).status(201).build();
+            return ResponseEntity.status(201).build();
         }
     }
 
-    public Response deleteStudentCourse(UUID courseUuid, UUID studentUuid) {
-        Course course = courseRepository.findByUuid(courseUuid).orElseThrow(() -> new IllegalStateException("course does not exist"));
-        Student student = studentRepository.findStudentByUuid(studentUuid).orElseThrow(() -> new IllegalStateException("student does not exist"));
+    public ResponseEntity<?> deleteStudentCourse(UUID courseUuid, UUID studentUuid) {
+        Course course = courseRepository.findByUuid(courseUuid).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "course does not exist"));
+        Student student = studentRepository.findStudentByUuid(studentUuid).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "student does not exist"));
         if (course.getStudents().stream().anyMatch(student1 -> student1.getUuid().equals(student.getUuid()))) {
             student.getCourses().removeIf(course1 -> course1.getUuid().equals(course.getUuid()));
             this.studentRepository.save(student);
-            return Response.status(204).build();
+            return ResponseEntity.status(204).build();
         }
-        return Response.noContent().status(406).build();
+        return ResponseEntity.noContent().build();
     }
 
-    public Response getStudentsCourse(UUID courseUuid) {
-        Course course = courseRepository.findByUuid(courseUuid).orElseThrow(() -> new IllegalStateException("course does not exist"));
-        return Response.ok(course.getStudents()).status(200).build();
+    public ResponseEntity<?> getStudentsCourse(UUID courseUuid) {
+        Course course = courseRepository.findByUuid(courseUuid).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "course does not exist"));
+        return ResponseEntity.ok(course.getStudents());
     }
 
 
-    public Response getCourseSearchQueryParam(String university, String campus, String name) {
-        if (!(university == null) && (campus == null) && (name == null)) {
-            Optional<Collection<Course>> courses = courseRepository.findByUniversityContaining(university);
-            return Response.ok(courses.get()).status(200).build();
-        } else if (!(campus == null) && (university == null) && (name == null)) {
-            Optional<Collection<Course>> courses = courseRepository.findByCampusContaining(campus);
-            return Response.ok(courses.get()).status(200).build();
-        } else if (!(name == null) && (university == null) && (campus == null)) {
-            Optional<Collection<Course>> courses = courseRepository.findByNameContaining(name);
-            return Response.ok(courses.get()).status(200).build();
-        } else if (!(university == null) && !(campus == null) && !(name == null)) {
-            return Response.ok().status(204).location(URI.create("https://www.w3schools.com/java/java_conditions.asp")).build();
-        } else if (!(university == null) && !(campus == null) && (name == null)) {
-            Optional<Collection<Course>> courses = courseRepository.findByUniversityContainingAndCampusContaining(university, campus);
-            return Response.ok(courses.get()).status(200).build();
-        } else if (!(university == null) && !(name == null) && (campus == null)) {
-            Optional<Collection<Course>> courses = courseRepository.findByUniversityContainingAndNameContaining(university, name);
-            return Response.ok(courses.get()).status(200).build();
-        } else if (!(name == null) && !(campus == null) && (university == null)) {
-            Optional<Collection<Course>> courses = courseRepository.findByNameContainingAndCampusContaining(name, campus);
-            return Response.ok(courses.get()).status(200).build();
-        }
-        return Response.noContent().build();
+    public ResponseEntity<?> getCourseSearchQueryParam(String university, String campus, String name) {
+//        if (!(university == null) && (campus == null) && (name == null)) {
+//            Optional<Collection<Course>> courses = courseRepository.findByUniversityContaining(university);
+//            return ResponseEntity.ok(courses.get()).status(200).build();
+//        } else if (!(campus == null) && (university == null) && (name == null)) {
+//            Optional<Collection<Course>> courses = courseRepository.findByCampusContaining(campus);
+//            return ResponseEntity.ok(courses.get()).status(200).build();
+//        } else if (!(name == null) && (university == null) && (campus == null)) {
+//            Optional<Collection<Course>> courses = courseRepository.findByNameContaining(name);
+//            return ResponseEntity.ok(courses.get()).status(200).build();
+//        } else if (!(university == null) && !(campus == null) && !(name == null)) {
+//            return ResponseEntity.ok(courses)
+//        } else if (!(university == null) && !(campus == null) && (name == null)) {
+//            Optional<Collection<Course>> courses = courseRepository.findByUniversityContainingAndCampusContaining(university, campus);
+//            return ResponseEntity.ok(courses.get()).status(200).build();
+//        } else if (!(university == null) && !(name == null) && (campus == null)) {
+//            Optional<Collection<Course>> courses = courseRepository.findByUniversityContainingAndNameContaining(university, name);
+//            return ResponseEntity.status(200).body(courses.get());
+//        } else if (!(name == null) && !(campus == null) && (university == null)) {
+//            Optional<Collection<Course>> courses = courseRepository.findByNameContainingAndCampusContaining(name, campus);
+//            return ResponseEntity.status(200).body((courses.get()));
+//        }
+        return ResponseEntity.notFound().build();
     }
 }
