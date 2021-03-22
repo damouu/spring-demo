8c8341f8c9e985b25937c47a232fc27febbe02be
damou_ <mouadsehbaoui@gmail.com>
2021-03-22 19:34:02 +0100

    refactor: BookController
    removes the RESTEASY implementation.
    
    #SD-100 Done Unassigned Fixed Done

--- src/main/java/com/example/demo/book/BookController.java
+++ src/main/java/com/example/demo/book/BookController.java
@@ -1,79 +1,61 @@
 package com.example.demo.book;
 
 import lombok.Data;
-import org.jboss.resteasy.annotations.jaxrs.PathParam;
 import org.springframework.http.MediaType;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import javax.ws.rs.*;
-import javax.ws.rs.core.Response;
 import java.util.Collection;
-import java.util.Optional;
 import java.util.UUID;
 
-@Validated
 @Data
-@Controller
-@Path("api/book")
+@Validated
+@RestController
+@RequestMapping("api/book")
 public class BookController {
 
     private final BookService bookService;
 
-    @GET
-    @Produces(MediaType.APPLICATION_JSON_VALUE)
-    public Collection<Book> getBooks(@DefaultValue("0") @QueryParam("page") int page, @DefaultValue("5") @QueryParam("size") int size,
-                                     @QueryParam("totalPages") Optional<Integer> totalPages, @QueryParam("genre") Optional<String> genre) {
-        return bookService.getBooks(page, size, totalPages, genre);
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public Collection<Book> getBooks(@RequestParam(name = "page", required = false, defaultValue = "0") int page,
+                                     @RequestParam(name = "size", required = false, defaultValue = "5") int size) {
+        return bookService.getBooks(page, size);
     }
 
-    @GET
-    @Path("/{bookUuid}")
-    @Produces(MediaType.APPLICATION_JSON_VALUE)
-    public Book getBookById(@PathParam("bookUuid") UUID bookUuid) {
+    @GetMapping(path = "/{bookUuid}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Book getBookById(@PathVariable("bookUuid") UUID bookUuid) {
         return bookService.getBookById(bookUuid);
     }
 
-    @POST
-    @Consumes(MediaType.APPLICATION_JSON_VALUE)
-    @Produces(MediaType.APPLICATION_JSON_VALUE)
-    public Response inertBook(@Valid Book book) {
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> inertBook(@Valid @RequestBody Book book) {
         return bookService.insertBook(book);
     }
 
-    @DELETE
-    @Path("/{bookUuid}")
-    public Response deleteBook(@PathParam("bookUuid") UUID bookUuid) {
+    @DeleteMapping(path = "/{bookUuid}")
+    public ResponseEntity<?> deleteBook(@PathVariable("bookUuid") UUID bookUuid) {
         return bookService.deleteBook(bookUuid);
     }
 
-    @PUT
-    @Path("/{bookUuid}")
-    @Consumes(MediaType.APPLICATION_JSON_VALUE)
-    @Produces(MediaType.APPLICATION_JSON_VALUE)
-    public Response updateBook(@PathParam("bookUuid") UUID bookUuid, @Valid Book book) {
+    @PutMapping(path = "/{bookUuid}", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> updateBook(@PathVariable("bookUuid") UUID bookUuid, @Valid Book book) {
         return bookService.updateBook(bookUuid, book);
     }
 
-    @POST
-    @Produces(MediaType.APPLICATION_JSON_VALUE)
-    @Path("/{book}/studentCard/{studentCard}")
-    public Response studentBorrowBooks(@PathParam("book") UUID bookUuid, @PathParam("studentCard") UUID studentUuidCard) {
+    @PostMapping(path = "/{book}/studentCard/{studentCard}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> studentBorrowBooks(@PathVariable("book") UUID bookUuid, @PathVariable("studentCard") UUID studentUuidCard) {
         return bookService.insertStudentBorrowBooks(bookUuid, studentUuidCard);
     }
 
-    @DELETE
-    @Produces(MediaType.APPLICATION_JSON_VALUE)
-    @Path("/{book}/studentCard/{studentCard}")
-    public Response returnStudentBorrowBooks(@PathParam("book") UUID bookUuid, @PathParam("studentCard") UUID studentUuidCard) {
+    @DeleteMapping(path = "/{book}/studentCard/{studentCard}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> returnStudentBorrowBooks(@PathVariable("book") UUID bookUuid, @PathVariable("studentCard") UUID studentUuidCard) {
         return bookService.returnStudentBorrowBooks(bookUuid, studentUuidCard);
     }
 
-    @GET
-    @Produces(MediaType.APPLICATION_JSON_VALUE)
-    @Path("/studentCard/{studentCard}")
-    public Response bookStudentBorrow(@PathParam("studentCard") UUID studentCard) {
+    @GetMapping(path = "/studentCard/{studentCard}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> bookStudentBorrow(@PathVariable("studentCard") UUID studentCard) {
         return bookService.getBooksStudentCard(studentCard);
     }
 }
