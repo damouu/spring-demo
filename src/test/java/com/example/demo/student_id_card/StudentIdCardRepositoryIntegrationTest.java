5848f08da1b2bee545c725f28766a5537ca79790
damou_ <mouadsehbaoui@gmail.com>
2021-03-24 18:44:38 +0100

    Refactor: StudentIdCardRepositoryIntegrationTest
    removes the @ExtendWith(SpringExtension.class) annotation, this annotation is to enable JUNIT 5 and Spring support however annotations like @DataJpaTest , @WebMvcTest and @SpringBootTest enables it for us.

--- src/test/java/com/example/demo/student_id_card/StudentIdCardRepositoryIntegrationTest.java
+++ src/test/java/com/example/demo/student_id_card/StudentIdCardRepositoryIntegrationTest.java
@@ -2,18 +2,15 @@ package com.example.demo.student_id_card;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.Optional;
 import java.util.UUID;
 
 @DataJpaTest
-@ExtendWith(SpringExtension.class)
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class StudentIdCardRepositoryIntegrationTest {
 
