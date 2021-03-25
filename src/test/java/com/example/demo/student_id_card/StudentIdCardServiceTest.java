dad984613590ef4ea57e4b58cf2af43d3df16595
damou_ <mouadsehbaoui@gmail.com>
2021-03-25 15:54:56 +0100

    Test: StudentIdCardServiceTest
    creates thr Unit test for getStudentIdCard function.
    
    #SD-110 Done Unassigned Fixed Done

--- src/test/java/com/example/demo/student_id_card/StudentIdCardServiceTest.java
+++ src/test/java/com/example/demo/student_id_card/StudentIdCardServiceTest.java
@@ -1,30 +1,26 @@
 package com.example.demo.student_id_card;
 
-import com.example.demo.student.StudentRepository;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.mockito.ArgumentCaptor;
-import org.mockito.Captor;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.UUID;
+
+@ExtendWith(MockitoExtension.class)
+// what does this annotation mean ?
 class StudentIdCardServiceTest {
 
     @Mock
+    // what does this annotation mean ?
     private StudentIdCardRepository studentIdCardRepository;
 
+    @InjectMocks
+    // what does this annotation mean ?
     private StudentIdCardService studentIdCardService;
-
-    private StudentRepository studentRepository;
-
-    @Captor
-    private ArgumentCaptor<StudentIdCard> argumentCaptor;
-
-    @BeforeEach
-    void setUp() {
-        MockitoAnnotations.initMocks(this);
-        studentIdCardService = new StudentIdCardService(studentIdCardRepository, studentRepository);
-    }
 
     @Test
     void getStudentIdCards() {
@@ -33,9 +29,31 @@ class StudentIdCardServiceTest {
 
     @Test
     void getStudentIdCard() {
+        StudentIdCard studentIdCard = new StudentIdCard(UUID.randomUUID());
+        Mockito.when(studentIdCardRepository.findStudentIdCardByUuid(studentIdCard.getUuid())).thenReturn(java.util.Optional.of(studentIdCard));
+        var studentIdCard1 = studentIdCardService.getStudentIdCard(studentIdCard.getUuid());
+        Assertions.assertNotNull(studentIdCard1);
+        Assertions.assertEquals(studentIdCard.getUuid(), studentIdCard1.getUuid());
+        Mockito.verify(studentIdCardRepository, Mockito.times(1)).findStudentIdCardByUuid(studentIdCard.getUuid());
     }
 
     @Test
-    void createStudentIdCard() {
+    void deleteStudentIdCard() {
+    }
+
+    @Test
+    void postStudentIdCard() {
+    }
+
+    @Test
+    void getStudentIdCardCourse() {
+    }
+
+    @Test
+    void getStudentIdCardRepository() {
+    }
+
+    @Test
+    void getStudentRepository() {
     }
 }
