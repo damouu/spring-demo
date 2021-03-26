95553ebc4497cb1e9a492222e50d3872f7b59028
damou_ <mouadsehbaoui@gmail.com>
2021-03-26 18:10:22 +0100

    changed the image build name so that it will push to the main docker hub damou/springdemo repository.

--- script.groovy
+++ script.groovy
@@ -18,7 +18,7 @@ def buildAppDocker() {
     sh 'mvn install'
     sh "cp /var/lib/jenkins/.m2/repository/com/example/demo/0.0.1-SNAPSHOT/demo-0.0.1-SNAPSHOT.jar /home/mouad/IdeaProjects/spring-demo/target/"
     docker.withRegistry('https://index.docker.io/v1/', 'DockerHub') {
-        def damouImage = docker.build("damou/springdemo-resteasy").push(params.tagImage)
+        def damouImage = docker.build("damou/springdemo").push(params.tagImage)
     }
     sh 'docker image prune -f'
 }
