5a281c02c8ce494d52b5b7c64643ccfa341c0a6a
damou_ <mouadsehbaoui@gmail.com>
2021-03-30 19:15:43 +0200

    Test: StudentControllerTest inertNewStudent
    Creates the Unit test for the inertNewStudent method.
    
    #SD-116 Done Unassigned Fixed Done

--- src/test/java/com/example/demo/student/StudentControllerTest.java
+++ src/test/java/com/example/demo/student/StudentControllerTest.java
@@ -1,10 +1,12 @@
 package com.example.demo.student;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MockMvc;
@@ -15,8 +17,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -28,6 +29,9 @@ class StudentControllerTest {
 
     @MockBean
     private StudentService studentService;
+
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @Test
     void allStudents() throws Exception {
@@ -57,7 +61,14 @@ class StudentControllerTest {
     }
 
     @Test
-    void inertNewStudent() {
+    void inertNewStudent() throws Exception {
+        UUID uuid = UUID.randomUUID();
+        Student student = new Student(uuid, "student", LocalDate.of(2000, Month.NOVEMBER, 1), "student@email.com");
+        Mockito.when(studentService.postStudent(student)).thenReturn(ResponseEntity.status(HttpStatus.CREATED).body(student));
+        mockMvc.perform(post("/api/student")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(student)))
+                .andExpect(status().is2xxSuccessful());
     }
 
     @Test
