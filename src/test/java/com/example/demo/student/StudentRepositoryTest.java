e970c304cd89970d1a0fd9d24506472bcd0fcb8a
damou_ <mouadsehbaoui@gmail.com>
2021-03-30 19:52:53 +0200

    StudentRepositoryTest add findStudentByUuid test.

--- src/test/java/com/example/demo/student/StudentRepositoryTest.java
+++ src/test/java/com/example/demo/student/StudentRepositoryTest.java
@@ -1,9 +1,15 @@
 package com.example.demo.student;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.Optional;
+import java.util.UUID;
 
 @DataJpaTest
 class StudentRepositoryTest {
@@ -20,5 +26,11 @@ class StudentRepositoryTest {
 
     @Test
     void findStudentByUuid() {
+        Student student = new Student(UUID.randomUUID(), "test", LocalDate.of(2000, Month.APRIL, 21), "test@hotmail.com");
+        entityManager.persist(student);
+        entityManager.flush();
+        Optional<Student> optional = studentRepository.findStudentByUuid(student.getUuid());
+        Assertions.assertFalse(optional.isEmpty());
+        Assertions.assertEquals(student.getUuid(), optional.get().getUuid());
     }
 }
