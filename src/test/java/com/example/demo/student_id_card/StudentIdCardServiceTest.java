854a3ae496953c90d76a5e612bb3156c44d71ab0
damou_ <mouadsehbaoui@gmail.com>
2021-04-06 16:32:56 +0200

    Test: StudentIdCardServiceTest deleteStudentIdCard
    creates the unit test deleteStudentIdCard for the service layer.
    
    #SD-126 Done Unassigned Fixed Done

--- src/test/java/com/example/demo/student_id_card/StudentIdCardServiceTest.java
+++ src/test/java/com/example/demo/student_id_card/StudentIdCardServiceTest.java
@@ -1,7 +1,6 @@
 package com.example.demo.student_id_card;
 
 import com.example.demo.student.Student;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -9,21 +8,19 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.ResponseEntity;
 
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.UUID;
 
 @ExtendWith(MockitoExtension.class)
-// what does this annotation mean ?
 class StudentIdCardServiceTest {
 
     @Mock
-    // what does this annotation mean ?
     private StudentIdCardRepository studentIdCardRepository;
 
     @InjectMocks
-    // what does this annotation mean ?
     private StudentIdCardService studentIdCardService;
 
 
@@ -44,6 +41,11 @@ class StudentIdCardServiceTest {
 
     @Test
     void deleteStudentIdCard() {
+        StudentIdCard studentIdCard = new StudentIdCard(UUID.randomUUID());
+        Mockito.when(studentIdCardRepository.findStudentIdCardByUuid(studentIdCard.getUuid())).thenReturn(java.util.Optional.of(studentIdCard));
+        ResponseEntity responseEntity = studentIdCardService.deleteStudentIdCard(studentIdCard.getUuid());
+        Assertions.assertTrue(responseEntity.getStatusCode().is2xxSuccessful());
+        Mockito.verify(studentIdCardRepository, Mockito.times(1)).delete(studentIdCard);
     }
 
     @Test
