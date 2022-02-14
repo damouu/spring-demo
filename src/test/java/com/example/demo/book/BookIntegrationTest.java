9dffae5a330e60c6021f2fc36fb7b3d4d4521218
damou_ <mouadsehbaoui@gmail.com>
2022-02-14 17:41:58 +0100

    BookIntegrationTest deleteBook IntegrationTest
    #SD-131 Done Unassigned Fixed Done

--- src/test/java/com/example/demo/book/BookIntegrationTest.java
+++ src/test/java/com/example/demo/book/BookIntegrationTest.java
@@ -31,8 +31,7 @@ public class BookIntegrationTest {
     void getBookUuid() {
         Book book = new Book(UUID.randomUUID(), "tittle", "genre", 200, "publisher", "author", LocalDate.now());
         bookRepository.save(book);
-        ResponseEntity<Book> responseEntity = restTemplate.getForEntity
-                ("http://localhost:" + port + "/api/book/" + book.getUuid(), Book.class);
+        ResponseEntity<Book> responseEntity = restTemplate.getForEntity("http://localhost:" + port + "/api/book/" + book.getUuid(), Book.class);
         Assertions.assertTrue(responseEntity.getStatusCode().is2xxSuccessful());
         Assertions.assertTrue(Objects.requireNonNull(responseEntity.getHeaders().getContentType()).isCompatibleWith(MediaType.APPLICATION_JSON));
         Assertions.assertEquals(Objects.requireNonNull(responseEntity.getBody()).getUuid(), book.getUuid());
@@ -41,12 +40,21 @@ public class BookIntegrationTest {
     @Test
     void postBook() {
         Book book = new Book(null, "tittle", "genre", 200, "publisher", "author", LocalDate.now());
-        ResponseEntity<Book> responseEntity = restTemplate.postForEntity
-                ("http://localhost:" + port + "/api/book", book, Book.class);
+        ResponseEntity<Book> responseEntity = restTemplate.postForEntity("http://localhost:" + port + "/api/book", book, Book.class);
         Optional<Book> book1 = bookRepository.findByUuid(Objects.requireNonNull(responseEntity.getBody()).getUuid());
         Assertions.assertEquals(201, responseEntity.getStatusCodeValue());
         Assertions.assertEquals(URI.create("http://localhost:8083/api/book/" + book1.get().getUuid()), responseEntity.getHeaders().getLocation());
         Assertions.assertEquals(responseEntity.getBody().getUuid(), book1.get().getUuid());
+    }
+
+    @Test
+    void deleteBook() {
+        Book book = new Book(UUID.randomUUID(), "DELETE", "DELETE", 200, "publisher", "author", LocalDate.now());
+        bookRepository.save(book);
+        restTemplate.delete("http://localhost:" + port + "/api/book/" + book.getUuid());
+        ResponseEntity<Book> responseEntity = restTemplate.getForEntity("http://localhost:" + port + "/api/book/" + book.getUuid(), Book.class);
+        Assertions.assertEquals(404, responseEntity.getStatusCodeValue());
+        Assertions.assertNotEquals(book.getUuid(), responseEntity.getBody().getUuid());
     }
 
 }
