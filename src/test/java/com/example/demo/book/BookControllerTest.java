3bc0de63c53749731c4936aa042e4d7b2426b8bf
damou_ <mouadsehbaoui@gmail.com>
2022-02-17 18:22:43 +0100

    Test updateBook
    #SD-133 Done Unassigned Fixed Done

--- src/test/java/com/example/demo/book/BookControllerTest.java
+++ src/test/java/com/example/demo/book/BookControllerTest.java
@@ -12,11 +12,11 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import java.net.URI;
 import java.time.LocalDate;
+import java.util.HashMap;
 import java.util.UUID;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(BookController.class)
 class BookControllerTest {
@@ -38,38 +38,37 @@ class BookControllerTest {
     void getBookUuid() throws Exception {
         Book book = new Book(UUID.randomUUID(), "tittle", "genre", 200, "publisher", "author", LocalDate.now());
         Mockito.when(bookService.getBookUuid(book.getUuid())).thenReturn(ResponseEntity.ok(book));
-        mockMvc.perform(get("/api/book/" + book.getUuid()))
-                .andExpect(status().is2xxSuccessful())
-                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
-                .andExpect(content().json("{\"uuid\":\"" + book.getUuid() + "\"," +
-                        "\"title\":\"" + book.getTitle() + "\"," +
-                        "\"genre\":\"" + book.getGenre() + "\"," +
-                        "\"totalPages\":" + book.getTotalPages() + "," +
-                        "\"publisher\":\"" + book.getPublisher() + "\"," +
-                        "\"author\":\"" + book.getAuthor() + "\"," +
-                        "\"created_at\":\"" + LocalDate.now() + "\"}"));
+        mockMvc.perform(get("/api/book/" + book.getUuid())).andExpect(status().is2xxSuccessful()).andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON)).andExpect(content().json("{\"uuid\":\"" + book.getUuid() + "\"," + "\"title\":\"" + book.getTitle() + "\"," + "\"genre\":\"" + book.getGenre() + "\"," + "\"totalPages\":" + book.getTotalPages() + "," + "\"publisher\":\"" + book.getPublisher() + "\"," + "\"author\":\"" + book.getAuthor() + "\"," + "\"created_at\":\"" + LocalDate.now() + "\"}"));
     }
 
     @Test
     void postBook() throws Exception {
         Book book = new Book(UUID.randomUUID(), "tittle", "genre", 200, "publisher", "author", LocalDate.now());
         Mockito.when(bookService.postBook(book)).thenReturn(ResponseEntity.status(201).location(URI.create("http://localhost:8083/api/book/" + book.getUuid())).body(book));
-        mockMvc.perform(post("/api/book/")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(book)))
-                .andExpect(status().is2xxSuccessful());
+        mockMvc.perform(post("/api/book/").contentType(MediaType.APPLICATION_JSON).content(objectMapper.writeValueAsString(book))).andExpect(status().is2xxSuccessful());
     }
 
     @Test
     void deleteBook() throws Exception {
         Book book = new Book(UUID.randomUUID(), "tittle", "genre", 200, "publisher", "author", LocalDate.now());
         Mockito.when(bookService.deleteBook(book.getUuid())).thenReturn(ResponseEntity.accepted().build());
-        mockMvc.perform(delete("/api/book/" + book.getUuid()))
-                .andExpect(status().is2xxSuccessful());
+        mockMvc.perform(delete("/api/book/" + book.getUuid())).andExpect(status().is2xxSuccessful());
     }
 
     @Test
-    void updateBook() {
+    void updateBook() throws Exception {
+        Book book = new Book(UUID.randomUUID(), "NON-UPDATED", "NON-UPDATED", 200, "NON-UPDATED", "NON-UPDATED", LocalDate.now());
+        Book bookUpdate = new Book(book.getUuid(), "UPDATED", "UPDATED", 500, "UPDATED", "UPDATED", LocalDate.now());
+        HashMap<String, String> bookUpdates = new HashMap<>();
+        bookUpdates.put("title", bookUpdate.getTitle());
+        bookUpdates.put("genre", bookUpdate.getGenre());
+        Mockito.when(bookService.updateBook(book.getUuid(), bookUpdates)).thenReturn(ResponseEntity.status(200).location(URI.create("http://localhost:8083/api/book/" + book.getUuid())).contentType(MediaType.APPLICATION_JSON).body(bookUpdate));
+        mockMvc.perform(put("/api/book/" + book.getUuid()).contentType(MediaType.APPLICATION_JSON).content(objectMapper.writeValueAsBytes(bookUpdates)))
+                .andExpect(status().is(200))
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(header().string("Location", "http://localhost:8083/api/book/" + bookUpdate.getUuid()))
+                .andExpect(content().json(objectMapper.writeValueAsString(bookUpdate)));
     }
 
     @Test
