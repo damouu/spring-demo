78b65ba21b3c600397ffe2687f96ed8df4c13771
damouu <mouadsehbaoui@gmail.com>
2022-09-27 21:32:05 +0900

    #SD-172 Done Unassigned Fixed Done
    implements the findTeacherByUuid unit test for the TeacherService class and the actual real function for TeacherService developed by using the TDD method.

--- src/main/java/com/example/demo/teacher/TeacherService.java
+++ src/main/java/com/example/demo/teacher/TeacherService.java
@@ -1,11 +1,19 @@
 package com.example.demo.teacher;
 
 import lombok.Data;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.UUID;
 
 @Data
 @Service
 public class TeacherService {
-    
+
     private final TeacherRepository teacherRepository;
+
+    public Teacher getTeacherUuid(UUID uuid) {
+        return teacherRepository.findTeacherByUuid(uuid).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "teacher not found"));
+    }
 }
