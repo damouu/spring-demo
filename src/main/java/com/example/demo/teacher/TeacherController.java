5ea00afb664638a9027117322fa6b8b6f45f4be6
damouu <mouadsehbaoui@gmail.com>
2022-10-01 18:26:08 +0900

    #SD-182 Done Unassigned Fixed Done
    Feature: TeacherController getTeacherUuid endpoint
    
    creates the endpoint to retrieve a teacher by the given uuid.

--- src/main/java/com/example/demo/teacher/TeacherController.java
+++ src/main/java/com/example/demo/teacher/TeacherController.java
@@ -1,14 +1,27 @@
 package com.example.demo.teacher;
 
 import lombok.Data;
+import org.springframework.http.MediaType;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 @Data
 @Validated
 @RestController
 @RequestMapping("api/teacher")
 public class TeacherController {
-    
+
+    // first, we do need the TeacherService bean, to be able to forward to the good method.
+    private final TeacherService teacherService;
+
+    @GetMapping(path = "/{teacherUuid}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Teacher getTeacherUuid(@PathVariable("teacherUuid") UUID teacherUuid) {
+        return teacherService.getTeacherUuid(teacherUuid);
+    }
+
 }
