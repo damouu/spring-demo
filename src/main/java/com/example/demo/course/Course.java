e75fcf66325f908af39b6055b9b234a388cff6ad
damouu <mouadsehbaoui@gmail.com>
2022-10-05 11:30:31 +0900

    #SD-184 Done Unassigned Fixed Done
    Refactor: Course Teacher property.
    
    Instead of implementing the annotations that would create a pivot table, I realized
    that a simple OneToneMany relationship between the two tables with a foreign key on the Course is what is the most appropriated for this context.

--- src/main/java/com/example/demo/course/Course.java
+++ src/main/java/com/example/demo/course/Course.java
@@ -1,6 +1,7 @@
 package com.example.demo.course;
 
 import com.example.demo.student_id_card.StudentIdCard;
+import com.example.demo.teacher.Teacher;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
@@ -51,15 +52,19 @@ public class Course {
     @Setter
     private String university;
 
-    @ManyToMany(fetch = FetchType.EAGER,
-            cascade = {
-                    CascadeType.PERSIST,
-                    CascadeType.ALL
-            },
-            mappedBy = "courses")
+    @ManyToMany(fetch = FetchType.EAGER, cascade = {CascadeType.PERSIST, CascadeType.ALL}, mappedBy = "courses")
     @Getter(onMethod = @__(@JsonIgnore))
     @Setter
     protected Set<StudentIdCard> studentIdCards = new HashSet<>();
+
+    /* so for HIbernate, you create the Table's columns just by adding properties to a class. as simple as that.
+    / the table's columns are most private properties whereas, shared properties/ columns are marked as protected.
+    / I've could picked the @ManuToOne annotation instead of the OneToMany but, as i am writing this from the viewpoint of the
+    Course Table i decided to go fot the OneToMany annotation */
+    @OneToMany(fetch = FetchType.EAGER, cascade = {CascadeType.PERSIST, CascadeType.ALL})
+    @Getter(onMethod = @__(@JsonIgnore))
+    @Setter
+    protected Set<Teacher> teacher;
 
     @JsonCreator
     public Course(@JsonProperty("uuid") UUID uuid, @JsonProperty("name") String name, @JsonProperty("campus") String campus, @JsonProperty("university") String university) {
