710db7228635d879404ebafa0bb275ffcecdca70
damouu <mouadsehbaoui@gmail.com>
2023-11-11 13:30:16 +0900

    #SD-213 Done Unassigned Fixed Done
    Feature: query Book containing
    
    creates a query to retrieve the books containing the same title passed as a parameter to the function.

--- src/main/java/com/example/demo/book/BookRepository.java
+++ src/main/java/com/example/demo/book/BookRepository.java
@@ -1,10 +1,13 @@
 package com.example.demo.book;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -13,6 +16,8 @@ public interface BookRepository extends JpaRepository<Book, Integer> {
 
     @Query("SELECT b FROM book b WHERE b.deleted_at is null and b.uuid = :uuid")
     Optional<Book> findByUuid(UUID uuid);
+
+    Optional<List<Book>> findByTitleContaining(@Param("title") String title, Pageable pageable);
 
     Optional<Collection<Book>> findAllByTotalPages(Optional<Integer> integer);
 
