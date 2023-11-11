467f6b07de71bda6dc85dc68c510437332438f08
damouu <mouadsehbaoui@gmail.com>
2023-11-11 13:26:49 +0900

    #SD-213 Done Unassigned Fixed Done
    Refactor: comments @Bean
    
    disables the @Bean spring annotation so Spring won't execute the commandLineRunner when booting the App.

--- src/main/java/com/example/demo/book/BookConfiguration.java
+++ src/main/java/com/example/demo/book/BookConfiguration.java
@@ -3,7 +3,6 @@ package com.example.demo.book;
 import com.example.demo.student.StudentRepository;
 import com.github.javafaker.Faker;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.time.LocalDate;
@@ -12,20 +11,12 @@ import java.util.UUID;
 
 @Configuration
 public class BookConfiguration {
-
-    @Bean
+    //    @Bean
     CommandLineRunner dedede(BookRepository bookRepository, StudentRepository studentRepository) {
         return args -> {
             Faker faker = new Faker();
             for (int i = 1; i < 100; i++) {
-                Book book = new Book(
-                        UUID.randomUUID(),
-                        faker.book().title(),
-                        faker.book().genre(),
-                        faker.number().numberBetween(1, 900),
-                        faker.book().publisher(),
-                        faker.book().author(),
-                        LocalDate.of(faker.number().numberBetween(1900, 2021), Month.NOVEMBER, faker.number().numberBetween(1, 31)));
+                Book book = new Book(UUID.randomUUID(), faker.book().title(), faker.book().genre(), faker.number().numberBetween(1, 900), faker.book().publisher(), faker.book().author(), LocalDate.of(faker.number().numberBetween(1900, 2021), Month.NOVEMBER, faker.number().numberBetween(1, 31)));
                 bookRepository.save(book);
             }
         };
