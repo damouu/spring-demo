73a60ee240e4a5b332fc2e65c3bcf306ccfa53fe
damouu <mouadsehbaoui@gmail.com>
2023-11-11 13:27:06 +0900

    #SD-213 Done Unassigned Fixed Done
    Refactor: comments @Bean
    
    disables the @Bean spring annotation so Spring won't execute the commandLineRunner when booting the App.

--- src/main/java/com/example/demo/teacher/TeacherConfiguration.java
+++ src/main/java/com/example/demo/teacher/TeacherConfiguration.java
@@ -2,7 +2,6 @@ package com.example.demo.teacher;
 
 import com.github.javafaker.Faker;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.text.SimpleDateFormat;
@@ -12,8 +11,8 @@ import java.util.UUID;
 @Configuration
 // tells to the spring framework that this class has a @Bean definition method..
 public class TeacherConfiguration {
-    @Bean
-        // tells to the spring that the method will return an object and it should be managed like a bean.
+    //    @Bean
+    // tells to the spring that the method will return an object and it should be managed like a bean.
     CommandLineRunner teacherRunner(TeacherRepository teacherRepository) {
         return args -> {
             Faker faker = new Faker();
